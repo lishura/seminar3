743b6038589b072f90f724c25bb123a017c3b50c
Alexandra Lisovskaya <lishura@rambler.ru>
2022-10-07 22:48:29 +0300

    add task19

---
+++ task19/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите пятизначное число");
+int x = Math.Abs(int.Parse(Console.ReadLine()));
+
+bool IsPalindrom(int x)
+{
+    if (x % 100 / 10 == x % 10000 / 1000 && x % 10 == x % 100000 / 10000)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+Console.WriteLine(IsPalindrom(x));
