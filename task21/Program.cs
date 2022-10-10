00e8e5c1c40ff6c260d04768aa4175d9a2af4d02
Alexandra Lisovskaya <lishura@rambler.ru>
2022-10-10 23:09:28 +0300

    add task21

---
+++ task21/Program.cs
@@ -0,0 +1,19 @@
+﻿double PrintAndGetValue(string message)
+{
+    Console.WriteLine(message);
+    string input = Console.ReadLine();
+    double value = double.Parse(input);
+    return value;
+}
+
+double xa = PrintAndGetValue("Введите координату х для точки А");
+double ya = PrintAndGetValue("Введите координату y для точки А");
+double za = PrintAndGetValue("Введите координату z для точки А");
+double xb = PrintAndGetValue("Введите координату х для точки B");
+double yb = PrintAndGetValue("Введите координату y для точки B");
+double zb = PrintAndGetValue("Введите координату z для точки B");
+
+
+double s = Math.Sqrt((Math.Pow((xa - xb), 2) + Math.Pow((ya - yb), 2) + Math.Pow((za - zb), 2)));
+
+Console.WriteLine(s);
